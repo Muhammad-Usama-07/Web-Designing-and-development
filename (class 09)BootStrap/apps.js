a853fdb77ebde5bb0dcdc394b890048ac8d7d4ff
Round Robin <Round@gmail.com>
2020-05-10 01:19:50 +0500

    Declearing and print variable html and javascript

--- (class 09)BootStrap/apps.js
+++ (class 09)BootStrap/apps.js
@@ -1,1 +1,12 @@
-alert("Hello World");
+//alert("Hello World");
+
+// var(keyword)
+// a(declearation)
+// =(Assigning operator)
+// 5(initialiazation)
+// ;(statement terminator)
+var a = 5;
+var b = 5;
+var c = a + b;
+
+alert(c)
