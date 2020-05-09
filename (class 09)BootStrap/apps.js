c068f1c98f04bbef2a4b51ed0ff5ac7a9b7c5f31
Round Robin <Round@gmail.com>
2020-05-10 01:52:54 +0500

    using post increament, pre increament,post decreament, pre decreament html and javascript

--- (class 09)BootStrap/apps.js
+++ (class 09)BootStrap/apps.js
@@ -21,8 +21,24 @@
 //var chatApp = 50;
 //alert(chatApp)
 
-var a = 3;
-var b = 5;
-var c = 7;
-var d = (b-a)*c;
-alert(d);
+//var a = 3;
+//var b = 5;
+//var c = 7;
+//var d = (b-a)*c;
+//alert(d);
+
+//using post increament, pre increament,post decreament, pre decreament.
+
+var a = 5;
+var b = a++;
+var c = ++a;
+var d = a--;
+var e = --a;
+
+alert("initial value: "+ a)
+alert("post increament: "+ b)
+alert("pre increament: "+ c)
+alert("post decreament: "+ d)
+alert("pre decreament: "+ e)
+
+
