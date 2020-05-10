f0c1988d8adb6b39da416275968e95729ee980ce
Round Robin <Round@gmail.com>
2020-05-10 15:46:19 +0500

    Using conditional statement in javascript

--- Class10/class10.js
+++ Class10/class10.js
@@ -2,8 +2,12 @@
 //var z = x++ + ++x + x + --x + x-- - x
 //alert(z)
 
-var x = "Enter value"
-var y = "your value is"
-var a  = +prompt(x, y)
-var b = 10+a;
-alert(b)
+//var x = "Enter value"
+//var y = "your value is"
+//var a  = +prompt(x, y)
+//var b = 10+a;
+//document.write("The output is: ",b) 
+
+//Conditional statement
+
+
