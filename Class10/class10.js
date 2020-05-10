1bfac2d66db9cfcc6b0bd5312d25500ae936605c
Round Robin <Round@gmail.com>
2020-05-10 15:59:34 +0500

    Using and operator statement in javascript

--- Class10/class10.js
+++ Class10/class10.js
@@ -9,12 +9,24 @@
 //document.write("The output is: ",b) 
 
 //Conditional statement
-var age = 26
-if(age<26){
-    alert("your age is less")
+//var age = 26
+//if(age==26){
+//    alert("your age is equal")
+//}
+//else if(age<26){
+ //   alert("your age age is less")
+//}
+//else{
+//    alert("your age is greater")
+//}
+
+var a  = +prompt("Enter Your age: ")
+var gender = prompt("Enter Your gender: ")
+if(a>18 && gender=="Male"){
+    alert("you are allowed")
 }
 else{
-    alert("your age is greater")
+    alert("you are not allowed")
 }
 
 
