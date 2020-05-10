2a293d6b3c43cd11102a7492db3d212d4d519740
Round Robin <Round@gmail.com>
2020-05-10 15:35:42 +0500

    Taking input from user in javascript

--- Class10/class10.js
+++ Class10/class10.js
@@ -1,3 +1,6 @@
-var x= 5;
-var z = x++ + ++x + x + --x + x-- - x
-alert(z)
+//var x= 5;
+//var z = x++ + ++x + x + --x + x-- - x
+//alert(z)
+
+var a  = prompt("Ënter your age", "your age is: ")
+alert(a)
