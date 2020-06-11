fe8e7fa284bcfcf7bf50c55bd6857d8ee46c84fe
Round Robin <Round@gmail.com>
2020-06-11 18:14:11 +0500

    Question 5 Chapter 2 done

--- Js Work and assign/Chapter 2/app.js
+++ Js Work and assign/Chapter 2/app.js
@@ -3,11 +3,16 @@
 //var message;
 //var message = "Hello World";
 //alert(message);
-var name = "Javed";
+/*var name = "Javed";
 var age = "15";
 var Skill = "Certified Mobile Application Development";
 alert(name);
 alert(age);
-alert(Skill);
-
+alert(Skill);*/
+var a = "PIZZA"
+alert(a + "\n" 
+      +a.substring(0,a.length -1)+"\n"
+      +a.substring(0,a.length -2)+"\n"
+      +a.substring(0,a.length -3)+"\n"
+      +a.substring(0,a.length -4));
 
