2aa8ffc8849ddcae87863c9bc0b345d3bcb2e6b6
Round Robin <Round@gmail.com>
2020-06-11 16:35:28 +0500

    Question 4 Chapter 2 done

--- Js Work and assign/Chapter 2/app.js
+++ Js Work and assign/Chapter 2/app.js
@@ -1,6 +1,13 @@
 //var username;
 //var myName = "Muhammad Usama";
 //var message;
-var message = "Hello World";
-alert(message);
+//var message = "Hello World";
+//alert(message);
+var name = "Javed";
+var age = "15";
+var Skill = "Certified Mobile Application Development";
+alert(name);
+alert(age);
+alert(Skill);
+
 
