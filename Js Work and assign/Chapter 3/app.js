6651ef81e9bfaae7f4a677512815294b62f53e70
Round Robin <Round@gmail.com>
2020-06-11 19:58:39 +0500

    Question 3 Chapter 3 done

--- Js Work and assign/Chapter 3/app.js
+++ Js Work and assign/Chapter 3/app.js
@@ -1,5 +1,8 @@
 /*var age = 15;
-alert("I am " + age+ " years old")*/
+alert("I am " + age+ " years old")
 var visit_time = 45;
-alert("you visit our website " + visit_time+ " Times")
+alert("you visit our website " + visit_time+ " Times")*/
+var birth_year = 1995;
+document.write("My birth year is "+birth_year+"<br>"+"Data types of mmy decleared variable is number");               
+
 
