d30c7a90dffd862745f2fa038d98d4aaacf4048b
Round Robin <Round@gmail.com>
2020-06-11 16:12:34 +0500

    Question 3 Chapter 1 done

--- Js Work and assign/Chapter 1/app.js
+++ Js Work and assign/Chapter 1/app.js
@@ -1,5 +1,7 @@
 //alert("Hay, Welcome to our website")
 
-alert("Error! Please enter a valid Password")
+//alert("Error! Please enter a valid Password")
+
+alert("Welcome to JS Land...\nHappy Coding!")
 
 
