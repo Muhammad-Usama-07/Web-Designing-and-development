7509275ef2b9e01b6fa9134d317f4911b9e0d0b9
Round Robin <Round@gmail.com>
2020-06-11 20:05:34 +0500

    Question 4 Chapter 3 done

--- Js Work and assign/Chapter 3/app.js
+++ Js Work and assign/Chapter 3/app.js
@@ -1,8 +1,13 @@
 /*var age = 15;
 alert("I am " + age+ " years old")
 var visit_time = 45;
-alert("you visit our website " + visit_time+ " Times")*/
+alert("you visit our website " + visit_time+ " Times")
 var birth_year = 1995;
-document.write("My birth year is "+birth_year+"<br>"+"Data types of mmy decleared variable is number");               
+document.write("My birth year is "+birth_year+"<br>"+"Data types of mmy decleared variable is number"); */
+var visiter_name = "John Doe";
+var product_title = "T-shirt(s)";
+var quantity = 5;
+document.write(visiter_name+" ordered "+ quantity+ product_title+" on XYZ Clothing store")
+
 
 
