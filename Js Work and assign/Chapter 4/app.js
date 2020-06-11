7fd9616c5203e829016eb48af11a20c8f37440bc
Round Robin <Round@gmail.com>
2020-06-11 20:54:33 +0500

    Question 3 Chapter 4 done

--- Js Work and assign/Chapter 4/app.js
+++ Js Work and assign/Chapter 4/app.js
@@ -15,7 +15,20 @@ var abd hdh = "Hello";
 var xe_as%ss$;
 var x, = "foot";
 var a = 1,2,3,3,3;*/
+var num = "number" 
+var dollar = "$"
+var a = "_";
+var lett = "Letter";
+var sen = "Sensitive";
+var key = "Keyword"
 
+document.write("<h3>Rules for naming JS variables</h3><br><br>")
+document.write("Variable names can only contain , "+num+" , "+dollar+" and "+a
+              +"For example $my_1stVariable<br>")
+document.write("Variables must begin with a "+lett+", "+dollar+"or"
+                +a+" For example $name, _name or name<br>")
+document.write("Variable names are case "+sen+"<br>")
+document.write("Variable names should not be JS "+key+"<br>")
 
 
 
