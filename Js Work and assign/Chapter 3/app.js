0844b1ac8b5bb889f35f344490cbc4b6af3ed29f
Round Robin <Round@gmail.com>
2020-06-11 19:49:53 +0500

    Question 2 Chapter 3 done

--- Js Work and assign/Chapter 3/app.js
+++ Js Work and assign/Chapter 3/app.js
@@ -1,4 +1,5 @@
-var age = 15;
-alert("I am " + age+ " years old")
-
+/*var age = 15;
+alert("I am " + age+ " years old")*/
+var visit_time = 45;
+alert("you visit our website " + visit_time+ " Times")
 
