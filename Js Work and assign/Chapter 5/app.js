d5512d73e466bb165e096e0ca6e353c477a013d7
Round Robin <Round@gmail.com>
2020-06-12 20:25:50 +0500

    Questionc 2 chapter 5 done

--- Js Work and assign/Chapter 5/app.js
+++ Js Work and assign/Chapter 5/app.js
@@ -1,4 +1,13 @@
 var a = prompt("Enter first number: ");
 var b = prompt("Enter second number: ");
-var result = parseInt(a)+parseInt(b);
-document.write("Sum of "+a+" and " +b+" is "+result)
+var addition = parseInt(a)+parseInt(b);   
+var subtraction = parseInt(a)-parseInt(b);   
+var multiplication = parseInt(a)*parseInt(b);   
+var Division = parseInt(a)/parseInt(b);   
+var mod = parseInt(a)%parseInt(b);   
+
+document.write("Sum of "+a+" and " +b+" is "+addition)
+document.write("<br>Difference of "+a+" and " +b+" is "+subtraction)
+document.write("<br>Multiplication of "+a+" and " +b+" is "+multiplication)
+document.write("<br>Division of "+a+" and " +b+" is "+Division)
+document.write("<br>Mode of "+a+" and " +b+" is "+mod)
