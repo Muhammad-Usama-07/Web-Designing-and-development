ee35ae3eb98bd24af16b0e05f13471137479709d
Round Robin <Round@gmail.com>
2020-06-13 14:22:53 +0500

    Question 6 Chapter 6 done

--- Js Work and assign/Chapter 6 -9/app.js
+++ Js Work and assign/Chapter 6 -9/app.js
@@ -21,7 +21,7 @@ document.write("<br>b is "+b)
 document.write("<br>result is "+result)
 
 var user = prompt("Enter Name:");
-alert("Welcome "+user);*/
+alert("Welcome "+user);
 
 var x = (prompt("Enter number: "));
 var a = 5;
@@ -34,4 +34,38 @@ for(var i =1; i<=10; i++){
     else{
         document.write(a+"x"+i+"="+a*i+"<br>")
     }
-}
+}*/
+
+var sub1 = prompt("Enter first Subject name: ")
+var sub2 = prompt("Enter second Subject name: ")
+var sub3 = prompt("Enter third Subject name: ")
+var obt_marks1 = parseInt(prompt("Enter obtained marks in "+sub1))
+var obt_marks2 = parseInt(prompt("Enter obtained marks in "+sub2))
+var obt_marks3 = parseInt(prompt("Enter obtained marks in "+sub3))
+var total_marks = 100;
+var percentage1 = (obt_marks1/total_marks)*100
+var percentage2 = (obt_marks1/total_marks)*100
+var percentage3 = (obt_marks1/total_marks)*100
+document.write("<h3>Subject Total Marks Otained Percentage</h3><br>")
+document.write(sub1+" "+total_marks+"      "+obt_marks1+"            "+percentage1+"%<br>");
+document.write(sub2+" "+total_marks+"      "+obt_marks2+"            "+percentage2+"%<br>");
+document.write(sub3+" "+total_marks+"      "+obt_marks3+"            "+percentage3+"%");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
