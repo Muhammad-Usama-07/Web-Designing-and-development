e34fd7b80d66cfcf1b5ad062f70f59d1b7071883
Round Robin <Round@gmail.com>
2020-06-13 14:57:21 +0500

    Question 5d chapter 9-11 done

--- Js Work and assign/Chapter 9 -11/app.js
+++ Js Work and assign/Chapter 9 -11/app.js
@@ -33,8 +33,8 @@ alert("given condition for variable a is true");
 var b = 82;
 if (b++ === 83){
 alert("given condition for variable b is true");
-}*/
 
+}
 
 var c = 12;
 if (c++ === 13){
@@ -48,6 +48,14 @@ alert("condition 3 is true");
 }
 if(c === 14){
 alert("condition 4 is true");
+}*/
+
+
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost){
+alert("The cost equals");
 }
 
 
