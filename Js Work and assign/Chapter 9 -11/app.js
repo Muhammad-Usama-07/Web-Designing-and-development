9652588ca6c1e98ac0bac29545c7e417fd28a1c1
Round Robin <Round@gmail.com>
2020-06-13 14:34:17 +0500

    Question 3 Chapter 9-11 done

--- Js Work and assign/Chapter 9 -11/app.js
+++ Js Work and assign/Chapter 9 -11/app.js
@@ -1,8 +1,25 @@
+/*var user = prompt("Enter city: ");
+if(user =="Karachi"){
+    alert("Welcome to the city of lights");
+}
+
 var user = prompt("Write your gender: ");
 if(user =="male"){
     alert("Good Morning sir");
 }
 else if(user =="female"){
     alert("Good Morning Ma'am");
+}*/
+
+var user = prompt("Enter Signal Colour: ");
+if(user =="Red"){
+    alert("Must be Stop");
 }
+else if(user =="Yellow"){
+    alert("Ready to move");
+}
+else if(user =="Green"){
+    alert("Move now");
+}
+
 
