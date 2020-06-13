0b018007ed3c688e93b17417429a483e9f261487
Round Robin <Round@gmail.com>
2020-06-13 13:16:01 +0500

    Question 2 Chapter 6 done

--- Js Work and assign/Chapter 6 -9/app.js
+++ Js Work and assign/Chapter 6 -9/app.js
@@ -1,4 +1,4 @@
-var a = 10;
+/*var a = 10;
 document.write("Result:<br>")
 document.write("The value of a is :"+a)
 document.write("<br>...................................<br><br>")
@@ -11,7 +11,11 @@ document.write("<br>Now the value of a is :"+a+"<br><br>")
 --a;
 document.write("The value of a is ++a:"+b)
 document.write("<br>Now the value of a is :"+a+"<br><br>")
-a--;
+a-- ;
 document.write("The value of a is ++a:"+b)
-document.write("<br>Now the value of a is :"+a+"<br><br>")
-
+document.write("<br>Now the value of a is :"+a+"<br><br>")*/
+var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
+document.write("a is "+a)
+document.write("<br>b is "+b)
+document.write("<br>result is "+result)
