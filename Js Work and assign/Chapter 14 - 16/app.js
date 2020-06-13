1fe672aec72d4759aeeff8b1568848d673c6b6ba
Round Robin <Round@gmail.com>
2020-06-13 17:38:32 +0500

    question 5 chapter 14-16 done

--- Js Work and assign/Chapter 14 - 16/app.js
+++ Js Work and assign/Chapter 14 - 16/app.js
@@ -1,4 +1,8 @@
-var arr = {"abc","asd","fffd","ddd"};
-var arr2 = {12,13,14,15};
+/*var arr = {"abc","asd","fffd","ddd"};
+var arr2 = {12,13,14,15};*/
 
-
+var arr3 = [];
+var num = 5;     
+for (var i = 0; i < num; i++) {
+  arr3.push(false);
+}
