1c7733c8318a892c68787ed751bb02ac0f7ffea8
Round Robin <Round@gmail.com>
2020-06-14 07:01:17 +0500

    Question 7 chapter 17-20 done

--- Js Work and assign/Chapter 17 - 20/app.js
+++ Js Work and assign/Chapter 17 - 20/app.js
@@ -25,7 +25,7 @@ var fruits = ["apple", "banana", "mango", "orange","strawberry"];
 
 for(var i = 0; i<fruits.length; i++){
     document.write("Element at index "+i+" is "+fruits[i]+"<br>")
-}*/
+}
 document.write("<h3>Counting</h3><br><br>");
 
 for(var i = 1; i<=15; i++){
@@ -59,8 +59,21 @@ for(var k = 1; k<=20; k++){
         document.write(k+"k, ");
     }
     
-}
+}*/
+var A = ["cake", "apple pie", "cookie", "chips", "patties"];
 
+var search = prompt("Welcome to Blue Ribbon bakery, What do you wnat to ordered ?");
+
+for(var i = 0; i<=A.length; i++){
+    if(search == A[i]){
+        document.write(A[i]+" is available at the index of "+i);
+        break;
+    }
+    else if(search != A[i]){
+        document.write("We are sorry "+search+" is not available in our Bakery");
+        break;
+    }
+}
 
 
 
