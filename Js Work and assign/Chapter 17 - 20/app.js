e99ebf63bd54d886b3f5d189057604adb5fd9030
Round Robin <Round@gmail.com>
2020-06-14 07:07:15 +0500

    Question 9 chapter 17-20 done

--- Js Work and assign/Chapter 17 - 20/app.js
+++ Js Work and assign/Chapter 17 - 20/app.js
@@ -59,7 +59,7 @@ for(var k = 1; k<=20; k++){
         document.write(k+"k, ");
     }
     
-}*/
+}
 var A = ["cake", "apple pie", "cookie", "chips", "patties"];
 
 var search = prompt("Welcome to Blue Ribbon bakery, What do you wnat to ordered ?");
@@ -73,10 +73,20 @@ for(var i = 0; i<=A.length; i++){
         document.write("We are sorry "+search+" is not available in our Bakery");
         break;
     }
+}*/
+
+var A = [24, 53, 78, 91, 12];
+
+document.write("Array items: "+A);
+var larg= 0;
+
+for (i=0; i<=larg;i++){
+    if (A[i]>larg) {
+        var larg=A[i];
+    }
 }
 
-
-
+document.write("<br>"+"The largest number is: "+larg)
 
 
 
