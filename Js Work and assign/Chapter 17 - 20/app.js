30e347ab573975a2b97279a9dbb075d1fcfb679d
Round Robin <Round@gmail.com>
2020-06-14 06:25:39 +0500

    Question 2 chapter 17-20 done

--- Js Work and assign/Chapter 17 - 20/app.js
+++ Js Work and assign/Chapter 17 - 20/app.js
@@ -1,1 +1,9 @@
-var arr = [[],[]];
+var arr = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+
+
+for(var i = 0; i < 4; i++){
+    for(var j = 0; j<4; j++){
+        document.write(arr[i][j]);
+    }
+    document.write("<br>")
+}
