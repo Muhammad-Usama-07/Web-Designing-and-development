93f096793f05750466c423dcef761e111560c915
Round Robin <Round@gmail.com>
2020-06-14 06:51:36 +0500

    Question 6 chapter 17-20 done

--- Js Work and assign/Chapter 17 - 20/app.js
+++ Js Work and assign/Chapter 17 - 20/app.js
@@ -18,16 +18,48 @@ lenght = parseInt(lenght);
 document.write("Multiplication of Table number "+table_num+" <br>Lenght "+lenght+"<br><br>")
 for(var i = 1; i <=lenght; i++){
     document.write(table_num+"x"+i+"="+table_num*i+"<br>");
-}*/
+}
 
 
 var fruits = ["apple", "banana", "mango", "orange","strawberry"];
 
 for(var i = 0; i<fruits.length; i++){
     document.write("Element at index "+i+" is "+fruits[i]+"<br>")
+}*/
+document.write("<h3>Counting</h3><br><br>");
+
+for(var i = 1; i<=15; i++){
+    document.write(i+", ");
 }
+document.write("<h3>Reverse</h3><br><br>");
 
+for(var j = 10; j>=1; j--){
+    document.write(j+", ");
+}
+document.write("<h3>Even</h3><br><br>");
 
+for(var k = 0; k<=20; k++){
+    if(k%2 == 0){
+        document.write(k+", ");
+    }
+    
+}
+document.write("<h3>Odd</h3><br><br>");
+
+for(var k = 0; k<=20; k++){
+    if(k%2 == 1){
+        document.write(k+", ");
+    }
+    
+}
+document.write("<h3>Series</h3><br><br>");
+
+for(var k = 1; k<=20; k++){
+    if(k%2 == 0){
+        document.write(k+"k, ");
+    }
+    
+}
 
 
 
