ccb9d354b40c96465c781a6c0ed48ab8cb191b39
Round Robin <Round@gmail.com>
2020-06-14 06:16:13 +0500

    Working in chapter 17-20

--- Js Work and assign/Chapter 14 - 16/app.js
+++ Js Work and assign/Chapter 14 - 16/app.js
@@ -5,7 +5,7 @@ var arr3 = [];
 var num = 5;     
 for (var i = 0; i < num; i++) {
   arr3.push(false);
-}*/
+}
 
 var arr4 = ["SSC", "HSC", "BCS",
 "BS", "BCOM", "MS", "M. Phil.", "PhD"];
@@ -13,4 +13,5 @@ var arr4 = ["SSC", "HSC", "BCS",
 document.write("<h1>Qualification</h1><br><br>");
 for(var i = 1; i<=arr4.length; i++){
     document.write(i+") "+arr4[i-1]+"<br>");
-}
+}*/
+
