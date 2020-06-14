a4b0c2fe1c8735b9ba3377e3dd6a78b98d77882f
Round Robin <Round@gmail.com>
2020-06-14 07:45:01 +0500

    Question 11 chapter 14-16 done

--- Js Work and assign/Chapter 14 - 16/app.js
+++ Js Work and assign/Chapter 14 - 16/app.js
@@ -13,8 +13,16 @@ var arr4 = ["SSC", "HSC", "BCS",
 document.write("<h1>Qualification</h1><br><br>");
 for(var i = 1; i<=arr4.length; i++){
     document.write(i+") "+arr4[i-1]+"<br>");
-}*/
+}
 var arr = [320,230,480,120];
 
 document.write("Score of Students: "+arr)
-document.write("<br>Score of Students: "+arr.sort())
+document.write("<br>Score of Students: "+arr.sort())*/
+
+var arr1 = ["karachi", "lahore", "islamabad", "quetta", "peshawar"]
+var arr2 = []
+document.write("Cities list: <br>"+arr1)
+for(var i = 2; i<=3;i++){
+    arr2.push(arr1[i])
+}
+document.write("<br>Selected Cities list: <br>"+arr2)
