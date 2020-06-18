1610129989f3154672220cd4712d620639758e01
Round Robin <Round@gmail.com>
2020-06-18 23:26:08 +0500

    / Creating head tail game

--- Js Work and assign/Class 13 Work/Chapter 26-30/app.js
+++ Js Work and assign/Class 13 Work/Chapter 26-30/app.js
@@ -8,9 +8,91 @@ var result = Math.round(a);
 var result2 = Math.ceil(a);
 var result3 = Math.floor(b);
 document.write(result+"\n"+result2+"\n"+result3);
-*/
+
 
 // generating random numbers
 
 document.write(Math.random())
+*/
+
+// Creating head tail game
+var headUser = prompt("Enter Head User: ");
+var tailUser = prompt("Enter Tail User: ");
+var toss = Math.random()*2;
+var floor = Math.floor(toss);
+if(floor==0){
+   alert("'tails' "+tailUser+" wins")
+   }
+else{
+    alert("'heads' "+headUser+" wins")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
