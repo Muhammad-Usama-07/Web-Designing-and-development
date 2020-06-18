6323d08d835d9100cfd9f69b032c1a63b0107c5d
Round Robin <Round@gmail.com>
2020-06-18 19:19:40 +0500

    Converting string to lower case

--- Js Work and assign/Class 13 Work/app.js
+++ Js Work and assign/Class 13 Work/app.js
@@ -1,7 +1,17 @@
+/*
+//Counting from 1 to 100
 for(var i =1; i<=100;i= i+10){
     
     for(var b=i; b<=i+9; b++){
         document.write(b + " ")
     }
         document.write("<br>")
-}
+}*/
+
+// Converting string to lower case
+
+var city = prompt("Enter city: ");
+city = city.toLowerCase();
+alert("u entered: "+city);
+
+
