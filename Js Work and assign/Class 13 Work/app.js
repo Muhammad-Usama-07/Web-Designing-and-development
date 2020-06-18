d1fe3ab07775fb33a9ed4b8b12f74d9f8163014d
Round Robin <Round@gmail.com>
2020-06-18 22:06:12 +0500

    Short method of Replacing sentence in a text program in java script.

--- Js Work and assign/Class 13 Work/app.js
+++ Js Work and assign/Class 13 Work/app.js
@@ -32,7 +32,7 @@ for(var i = 0; i< numChar; i++)
         alert("NO double Spaces!");
         break;
     }
-}*/
+}
 
 // Replacing sentence in a text program in java script.
 var text = "The New Yorker magazine doesn't allow the phrase "+"World War II. "+" They say it should be "+"the Second World War." +" So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers. It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his. "
@@ -44,7 +44,12 @@ text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
 }
 
 document.write(text);
-
+*/
+// Short method of Replacing sentence in a text program in java script.
+var text = "The New Yorker magazine doesn't allow the phrase 'World War II. ' They say it should be 'the Second World War.' So let's search the following sentence for the banned characters and replace them with the phrase that the New Yorker prefers."
+var indexx = text.indexOf("World War II");
+var new_text = text.slice(0,indexx) + 'The Second World War' +text.slice(indexx+12);
+document.write(new_text);
 
 
 
