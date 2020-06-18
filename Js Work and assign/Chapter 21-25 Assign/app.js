9e9f789ccb3877311d130ce4ab12d495de6ab452
Round Robin <Round@gmail.com>
2020-06-19 00:00:10 +0500

    Question # 1 Chapter 21-25 Done

---
+++ Js Work and assign/Chapter 21-25 Assign/app.js
@@ -0,0 +1,6 @@
+// Question # 1
+
+var user1 = prompt("Enter your first name: ");
+var user2 = prompt("Enter last first name: ");
+var title = "Welcome "+user1+" "+user2+" to our website.";
+alert(title);
