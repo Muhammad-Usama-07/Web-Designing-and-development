127b6724b25649c498d84e536939eff1420b26f2
Round Robin <Round@gmail.com>
2020-06-18 19:23:07 +0500

    Converting string to upper case

--- Js Work and assign/Class 13 Work/app.js
+++ Js Work and assign/Class 13 Work/app.js
@@ -6,12 +6,18 @@ for(var i =1; i<=100;i= i+10){
         document.write(b + " ")
     }
         document.write("<br>")
-}*/
+}
 
 // Converting string to lower case
 
 var city = prompt("Enter city: ");
 city = city.toLowerCase();
 alert("u entered: "+city);
+*/
 
+// Converting string to upper case
+
+var city = prompt("Enter city: ");
+city = city.toUpperCase();
+alert("u entered: "+city);
 
