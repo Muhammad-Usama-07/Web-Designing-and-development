a18d810df33a21d63d3770de9bfd2946f113cbe2
Round Robin <Round@gmail.com>
2020-06-18 21:44:32 +0500

    Check for double space program

--- Js Work and assign/Class 13 Work/app.js
+++ Js Work and assign/Class 13 Work/app.js
@@ -13,11 +13,49 @@ for(var i =1; i<=100;i= i+10){
 var city = prompt("Enter city: ");
 city = city.toLowerCase();
 alert("u entered: "+city);
-*/
+
 
 // Converting string to upper case
 
 var city = prompt("Enter city: ");
 city = city.toUpperCase();
 alert("u entered: "+city);
+
+
+
+// Check for double space program
+var str = prompt("Enter some text: ");
+var numChar = str.length;
+for(var i = 0; i< numChar; i++)
+{
+    if(str.slice(i, i+2)==="  "){
+        alert("NO double Spaces!");
+        break;
+    }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
