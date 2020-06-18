b44645e701f465936f6cf365d3cafef2bd3dde27
Round Robin <Round@gmail.com>
2020-06-18 23:05:51 +0500

    / Using math.ciel and math.floor

--- Js Work and assign/Class 13 Work/Chapter 26-30/app.js
+++ Js Work and assign/Class 13 Work/Chapter 26-30/app.js
@@ -3,5 +3,8 @@
 
 // Using math.round 
 var a = 1.3;
+var b = 1.6;
 var result = Math.round(a);
-document.write(result)
+var result2 = Math.ceil(a);
+var result3 = Math.floor(b);
+document.write(result+"\n"+result2+"\n"+result3);
