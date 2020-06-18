1f9b4a0730df97990170e96b9c4a2d0c5e924503
Round Robin <Round@gmail.com>
2020-06-18 18:32:16 +0500

    Working in class 13

--- Js Work and assign/Chapter 14 - 16/app.js
+++ Js Work and assign/Chapter 14 - 16/app.js
@@ -1,75 +1,115 @@
-/*var arr = {"abc","asd","fffd","ddd"};
-var arr2 = {12,13,14,15};
+/*var arr = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
 
-var arr3 = [];
-var num = 5;     
-for (var i = 0; i < num; i++) {
-  arr3.push(false);
+
+for(var i = 0; i < 4; i++){
+    for(var j = 0; j<4; j++){
+        document.write(arr[i][j]);
+    }
+    document.write("<br>")
 }
 
-var arr4 = ["SSC", "HSC", "BCS",
-"BS", "BCOM", "MS", "M. Phil.", "PhD"];
-
-document.write("<h1>Qualification</h1><br><br>");
-for(var i = 1; i<=arr4.length; i++){
-    document.write(i+") "+arr4[i-1]+"<br>");
+for(var i = 1; i<=10; i++){
+    document.write(i+"<br>")
 }
-var arr = [320,230,480,120];
 
-document.write("Score of Students: "+arr)
-document.write("<br>Score of Students: "+arr.sort())
-
-var arr1 = ["karachi", "lahore", "islamabad", "quetta", "peshawar"]
-var arr2 = []
-document.write("Cities list: <br>"+arr1)
-for(var i = 2; i<=3;i++){
-    arr2.push(arr1[i])
-}
-document.write("<br>Selected Cities list: <br>"+arr2)
-
-
-var arr = ["This "," is ", " my ", " cat"];
-document.write("Array: <br>"+arr+"<br><br>");
-var sstring = arr.join("");
-document.write("String: <br>"+sstring);
-
-var arr = ["Keyboard","Mouse", "Printer", "Moniter"];
-document.write("Array: <br>"+arr+"<br><br>");
-var arr2 = [];
-
-for(var i = 0 ; i<=3; i++){
-    arr2.push(arr[i]);
-}
-for(var i = 0 ; i<=3; i++){
-    document.write("Out:<br>"+arr2[i]+"<br>")
-}*/
-
-
-var arr = ["Keyboard","Mouse", "Printer", "Moniter"];
-document.write("Array: <br>"+arr+"<br><br>");
-var arr2 = [];
-
-for(var i = 3 ; i>=0; i--){
-    arr2.push(arr[i]);
-}
-for(var i = 0 ; i<=3; i++){
-    document.write("Out:<br>"+arr2[i]+"<br>")
+var table_num = prompt("Enter Table number: ");
+var lenght = prompt("Enter lenght: ")
+lenght = parseInt(lenght);
+document.write("Multiplication of Table number "+table_num+" <br>Lenght "+lenght+"<br><br>")
+for(var i = 1; i <=lenght; i++){
+    document.write(table_num+"x"+i+"="+table_num*i+"<br>");
 }
 
 
+var fruits = ["apple", "banana", "mango", "orange","strawberry"];
+
+for(var i = 0; i<fruits.length; i++){
+    document.write("Element at index "+i+" is "+fruits[i]+"<br>")
+}
+document.write("<h3>Counting</h3><br><br>");
+
+for(var i = 1; i<=15; i++){
+    document.write(i+", ");
+}
+document.write("<h3>Reverse</h3><br><br>");
+
+for(var j = 10; j>=1; j--){
+    document.write(j+", ");
+}
+document.write("<h3>Even</h3><br><br>");
+
+for(var k = 0; k<=20; k++){
+    if(k%2 == 0){
+        document.write(k+", ");
+    }
+    
+}
+document.write("<h3>Odd</h3><br><br>");
+
+for(var k = 0; k<=20; k++){
+    if(k%2 == 1){
+        document.write(k+", ");
+    }
+    
+}
+document.write("<h3>Series</h3><br><br>");
+
+for(var k = 1; k<=20; k++){
+    if(k%2 == 0){
+        document.write(k+"k, ");
+    }
+    
+}
+var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+var search = prompt("Welcome to Blue Ribbon bakery, What do you wnat to ordered ?");
+
+for(var i = 0; i<=A.length; i++){
+    if(search == A[i]){
+        document.write(A[i]+" is available at the index of "+i);
+        break;
+    }
+    else if(search != A[i]){
+        document.write("We are sorry "+search+" is not available in our Bakery");
+        break;
+    }
+}
+
+var A = [24, 53, 78, 91, 12];
+
+document.write("Array items: "+A);
+var larg= 0;
+
+for (i=0; i<=larg;i++){
+    if (A[i]>larg) {
+        var larg=A[i];
+    }
+}
+
+document.write("<br>"+"The largest number is: "+larg)
+
+var A = [24, 53, 78, 91, 12];
+
+document.write("Array items: "+A);
+var larg= A[0];
+
+for (i=0; i<=larg;i++){
+    if (A[i]<larg) {
+        var larg=A[i];
+    }
+}
+
+document.write("<br>"+"The Smallest number is: "+larg)
 
 
+document.write("Here Multiples of 5 ranging 1 to 100.<br><br>");
+for ( var i=1; i<=100;i++){
+    if (i%5 == 0) {
+        document.write(i+", ")
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 
