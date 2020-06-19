c1523c16df03432a68fbae6f4320e0d91432164a
Round Robin <Round@gmail.com>
2020-06-19 12:44:41 +0500

    Question 2 Chapter 21-25 done

--- Js Work and assign/Chapter 21-25 Assign/app.js
+++ Js Work and assign/Chapter 21-25 Assign/app.js
@@ -1,6 +1,23 @@
 // Question # 1
-
+/*
 var user1 = prompt("Enter your first name: ");
 var user2 = prompt("Enter last first name: ");
 var title = "Welcome "+user1+" "+user2+" to our website.";
 alert(title);
+*/
+// Question # 2
+var phone = prompt("Enter your favorite mobile model: ");
+document.write("Your Favorite mobile model is: "+phone+"<br>Leght of String: "+phone.length)
+// Question # 1
+// Question # 1
+// Question # 1
+// Question # 1
+// Question # 1
+// Question # 1
+// Question # 1
+// Question # 1
+// Question # 1
+
+
+
+
