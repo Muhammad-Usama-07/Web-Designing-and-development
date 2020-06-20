cba959acbc6e163549d28ad7e96723932296da52
Round Robin <Round@gmail.com>
2020-06-20 08:40:25 +0500

    Question 2 chapter 26-30 done

--- Js and work (part2)/Chapter 26-30 Assign/app.js
+++ Js and work (part2)/Chapter 26-30 Assign/app.js
@@ -1,4 +1,13 @@
+/*// Question # 1
 var num = 3.45214;
+document.write("number: "+num+"<br>");
+document.write("round off value: "+Math.round(num)+"<br>");
+document.write("floor value: "+Math.floor(num)+"<br>");
+document.write("ceil value: "+Math.ceil(num));
+*/
+
+// Question # 2
+var num = -2.673;
 document.write("number: "+num+"<br>");
 document.write("round off value: "+Math.round(num)+"<br>");
 document.write("floor value: "+Math.floor(num)+"<br>");
