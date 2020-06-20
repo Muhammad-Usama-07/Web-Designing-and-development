e1260ee299432ad9a7a09a8377e2cf79d5de7ba2
Round Robin <Round@gmail.com>
2020-06-20 08:36:31 +0500

    Working Assignment part 2

---
+++ Js and work (part2)/Chapter 26-30 Assign/app.js
@@ -0,0 +1,5 @@
+var num = 3.45214;
+document.write("number: "+num+"<br>");
+document.write("round off value: "+Math.round(num)+"<br>");
+document.write("floor value: "+Math.floor(num)+"<br>");
+document.write("ceil value: "+Math.ceil(num)+"");
