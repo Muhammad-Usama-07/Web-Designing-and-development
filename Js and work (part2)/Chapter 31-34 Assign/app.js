d2541b5fe5afc44cf8078c9273018d46f1ccd3ff
Round Robin <Round@gmail.com>
2020-06-20 11:25:45 +0500

    Question 3 chapter 31-34 done

--- Js and work (part2)/Chapter 31-34 Assign/app.js
+++ Js and work (part2)/Chapter 31-34 Assign/app.js
@@ -1,7 +1,7 @@
 /*// Question # 1
 var date = new Date();
 document.write(date);
-*/
+
 // Question # 2
 var month = new Date();
 var current_month = month.getMonth();
@@ -9,7 +9,14 @@ var current_month = month.getMonth();
 if(current_month == 5){
     alert("Current month: June")
 }
-// Question # 1
+*/
+
+// Question # 3
+var current = new Date();
+var current_day = current.getDay();
+var day_list = ["Sun", "Mon","Tue","Wed","Thus","Fri","Sat"]
+alert("Today is "+day_list[current_day])
+
 // Question # 1
 // Question # 1
 // Question # 1
