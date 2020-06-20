6e77a0782f5d0049f88caa275bb5ae7de9b023cc
Round Robin <Round@gmail.com>
2020-06-20 14:51:43 +0500

    Question 4 chapter 35-38 done

--- Js and work (part2)/Chapter 34-38 Assign/app.js
+++ Js and work (part2)/Chapter 34-38 Assign/app.js
@@ -10,17 +10,52 @@ function greeting(var1, var2){
 var firstname = prompt("Enter first name: ");
 var lastname = prompt("Enter last name: ");
 greeting(firstname,lastname);
-*/
+
 // Question # 03
 function Addition(var1, var2){
     alert("The addition of the numbers is: "+(var1+ var2));
 }
-var firstname = prompt("Enter first name: ");
+var firstname = prompt("Enter first number: ");
 firstname = parseInt(firstname);
-var lastname = prompt("Enter last name: ");
+var lastname = prompt("Enter last number: ");
 lastname = parseInt(lastname)
 Addition(firstname,lastname);
+*/
 // Question # 04
+function addition(var1, var2, operator){
+    var1 = parseInt(var1);
+    var2 = parseInt(var2);
+    switch(operator){
+        case "+":
+            var result = var1+var2;
+            alert("The addition of "+var1+" and "+var2+" is "+result);
+            break;
+        case "-":
+            var result = var1-var2;
+            alert("The subtraction from of "+var1+" and "+var2+" is "+result);
+            break;
+        case "*":
+            var result = var1*var2;
+            alert("The Multiplication of "+var1+" and "+var2+" is "+result);
+            break;
+        case "/":
+            var result = var1/var2;
+            alert("The Division from "+var1+" and "+var2+" is "+result);
+            break;
+        case "%":
+            var result = var1%var2;
+            alert("The mode from "+var1+" and "+var2+" is "+result);
+            break;
+           
+           }
+}
+
+var operator = prompt("Enter operator: \n1. +\n2. -\n3. *\n4. /\n5. %");
+var firstnumber = prompt("Enter first number: ");
+var lastnumber = prompt("Enter last number: ");
+firstnumber = parseInt(firstnumber);
+firstnumber = parseInt(firstnumber);
+addition(firstnumber,lastnumber, operator);
 // Question # 05
 // Question # 06
 // Question # 07
