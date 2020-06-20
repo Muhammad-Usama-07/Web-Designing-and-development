2d8db653d97b8edb7ff26e4fda3ffe4b58df44d2
Round Robin <Round@gmail.com>
2020-06-20 11:21:41 +0500

    Question 2 chapter 31-34 done

--- Js and work (part2)/Chapter 31-34 Assign/app.js
+++ Js and work (part2)/Chapter 31-34 Assign/app.js
@@ -1,2 +1,17 @@
+/*// Question # 1
 var date = new Date();
 document.write(date);
+*/
+// Question # 2
+var month = new Date();
+var current_month = month.getMonth();
+
+if(current_month == 5){
+    document.write("Current month: June")
+}
+// Question # 1
+// Question # 1
+// Question # 1
+// Question # 1
+// Question # 1
+// Question # 1
