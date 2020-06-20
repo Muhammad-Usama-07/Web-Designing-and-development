33cfeed881cb66df5a92f9481130f839d5e77f90
Round Robin <Round@gmail.com>
2020-06-20 08:43:45 +0500

    Question 3 chapter 26-30 done

--- Js and work (part2)/Chapter 26-30 Assign/app.js
+++ Js and work (part2)/Chapter 26-30 Assign/app.js
@@ -4,7 +4,7 @@ document.write("number: "+num+"<br>");
 document.write("round off value: "+Math.round(num)+"<br>");
 document.write("floor value: "+Math.floor(num)+"<br>");
 document.write("ceil value: "+Math.ceil(num));
-*/
+
 
 // Question # 2
 var num = -2.673;
@@ -12,3 +12,16 @@ document.write("number: "+num+"<br>");
 document.write("round off value: "+Math.round(num)+"<br>");
 document.write("floor value: "+Math.floor(num)+"<br>");
 document.write("ceil value: "+Math.ceil(num));
+*/
+
+// Question # 3
+var num = -4;
+document.write("The absolute value of "+num+" is "+Math.abs(num));
+// Question # 3
+// Question # 3
+// Question # 3
+// Question # 3
+// Question # 3
+// Question # 3
+// Question # 3
+
