75cd659e66b62acf6b6425a6e750d5ca8df1ea04
Round Robin <Round@gmail.com>
2020-06-20 14:32:13 +0500

    Question 2 chapter 35-38 done

--- Js and work (part2)/Chapter 34-38 Assign/app.js
+++ Js and work (part2)/Chapter 34-38 Assign/app.js
@@ -3,3 +3,22 @@
 var current = new Date();
 alert(current);
 */
+// Question # 02
+function greeting(var1, var2){
+    alert("Welcome "+var1+" "+var2);
+}
+var firstname = prompt("Enter first name: ");
+var lastname = prompt("Enter last name: ");
+greeting(firstname,lastname);
+// Question # 03
+// Question # 04
+// Question # 05
+// Question # 06
+// Question # 07
+// Question # 08
+// Question # 09
+// Question # 10
+// Question # 11
+// Question # 12
+// Question # 13
+// Question # 14
