5b27e863f65bdf2f4fb2ed9ae38a6ddecb7e3e97
Round Robin <Round@gmail.com>
2020-06-26 21:49:06 +0500

    working in Javascript with clear buttons

--- Js and work (part2)/Calculator with UI/aap.js
+++ Js and work (part2)/Calculator with UI/aap.js
@@ -2,3 +2,7 @@ function getNumber(num){
     var result = document.getElementById('result');
     result.value += num;
 }
+function clearResult(){
+    var result = document.getElementById("result");
+    result.value = "";
+}
