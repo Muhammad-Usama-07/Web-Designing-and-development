0db416ba8f19b04f00b6b963d57cde997fc941d8
Round Robin <Round@gmail.com>
2020-06-26 21:51:36 +0500

    working in on/off button in Javascript and on and off function

--- Js and work (part2)/Calculator with UI/aap.js
+++ Js and work (part2)/Calculator with UI/aap.js
@@ -1,3 +1,13 @@
+function on(){
+    var result = document.getElementById('result');
+    result.style.backgroundColor = 'white' ;
+    result.value = "";
+}
+function of(){
+    var result = document.getElementById('result');
+    result.style.backgroundColor = '737373' ;
+    result.value = "";
+}
 function getNumber(num){
     var result = document.getElementById('result');
     result.value += num;
