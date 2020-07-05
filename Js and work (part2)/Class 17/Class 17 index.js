f7fda908c26e65fa77eda09eb8c8b574bab30d59
Round Robin <Round@gmail.com>
2020-07-05 20:37:11 +0500

    Stopping steInterval function

--- Js and work (part2)/Class 17/Class 17 index.js
+++ Js and work (part2)/Class 17/Class 17 index.js
@@ -7,10 +7,40 @@ function timer(){
     document.write("runing<br>");
 }
 setInterval(timer,1000)
-*/
+
 // using predefine stop function
     // stop function to 3 second
 function timeout(){
     document.write("runing<br>");
 }
 setTimeout(timeout,3000);
+*/
+// Stopping steInterval function
+var count = 0;
+var interval;
+function timer(){
+    count++;
+    document.write(count+"<br>");
+}
+interval = setInterval(timer,1000);
+
+setTimeout(function(){
+    clearInterval(interval);
+},3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
