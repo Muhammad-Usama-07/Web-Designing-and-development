29a7e4f92619389a7e5417eb9b0612f625e3e35a
Round Robin <Round@gmail.com>
2020-07-05 20:51:22 +0500

    setting for milisec and sec

--- Js and work (part2)/Class 17/Class 17 index.js
+++ Js and work (part2)/Class 17/Class 17 index.js
@@ -29,6 +29,27 @@ setTimeout(function(){
 },3000);
 */
 
+// Making stop watch
+
+var min = 0;
+var sec = 0;
+var msec = 0;
+var minheading  = document.getElementById('min');
+var secheading  = document.getElementById('sec');
+var msecheading  = document.getElementById('msec');
+var interval;
+
+function timer(){
+    msec++;
+    msecheading.innerHTML = msec;
+    if(msec >= 100){
+       sec++;
+        secheading.innerHTML = sec;
+        msec = 0;
+       }
+}
+interval = setInterval(timer, 10)
+
 
 
 
