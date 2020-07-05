c4fd7159ade499ca7b3b62a171dec2400c8244a9
Round Robin <Round@gmail.com>
2020-07-05 20:32:26 +0500

    stop function to 3 second

--- Js and work (part2)/Class 17/Class 17 index.js
+++ Js and work (part2)/Class 17/Class 17 index.js
@@ -2,7 +2,15 @@
 
 // using predefine repeativelly function
     // running function to 1 second
+/*
 function timer(){
     document.write("runing<br>");
 }
 setInterval(timer,1000)
+*/
+// using predefine stop function
+    // stop function to 3 second
+function timeout(){
+    document.write("runing<br>");
+}
+setTimeout(timeout,3000);
