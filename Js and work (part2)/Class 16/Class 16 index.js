6d35402b14f41d0562ced36917f0286d5484df78
Round Robin <Round@gmail.com>
2020-07-05 06:49:07 +0500

    making read more in paragraph(method 2)

--- Js and work (part2)/Class 16/Class 16 index.js
+++ Js and work (part2)/Class 16/Class 16 index.js
@@ -1,7 +1,7 @@
 // Starting Chapter 51
 
 function readmore(){
-    var text = "sdfsd sdsdsdsd fggdversd wewes wewss  wewe  wewewc  wewew xcwegrgd wewewcsw  wewe  we w w w wewe we we we we we weeedsds  ";
+    var text = document.getElementById('text').innerHTML;
     var para = document.getElementById("para");
     para.innerHTML = text;
 }
