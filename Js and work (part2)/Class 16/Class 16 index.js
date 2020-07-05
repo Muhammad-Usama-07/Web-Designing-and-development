b21840ec5db9929123a18073ada8374a618451bd
Round Robin <Round@gmail.com>
2020-07-05 06:51:02 +0500

    using list in read more in paragraph

--- Js and work (part2)/Class 16/Class 16 index.js
+++ Js and work (part2)/Class 16/Class 16 index.js
@@ -1,7 +1,7 @@
 // Starting Chapter 51
 
 function readmore(){
-    var text = document.getElementById('text').innerHTML;
+    var text = '<ul><li>li</li><li>li</li><li>li</li><li>li</li></ul>';
     var para = document.getElementById("para");
     para.innerHTML = text;
 }
