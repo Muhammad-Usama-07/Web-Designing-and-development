699d1bdc1096ea1a038ef21bc549d956bb93bb5f
Round Robin <Round@gmail.com>
2020-07-05 20:30:00 +0500

    running function to 1 second

--- Js and work (part2)/Class 17/Class 17 index.js
+++ Js and work (part2)/Class 17/Class 17 index.js
@@ -1,1 +1,8 @@
 // Starting class 17
+
+// using predefine repeativelly function
+    // running function to 1 second
+function timer(){
+    document.write("runing<br>");
+}
+setInterval(timer,1000)
