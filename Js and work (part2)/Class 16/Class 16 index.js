6701f5e9a0b63a345a692be8e4ce2868bf009abc
Round Robin <Round@gmail.com>
2020-07-05 06:52:29 +0500

    Starting Chapter 52

--- Js and work (part2)/Class 16/Class 16 index.js
+++ Js and work (part2)/Class 16/Class 16 index.js
@@ -1,7 +1,11 @@
 // Starting Chapter 51
-
+/*
 function readmore(){
     var text = '<ul><li>li</li><li>li</li><li>li</li><li>li</li></ul>';
     var para = document.getElementById("para");
     para.innerHTML = text;
-}
+}*/
+
+// Starting Chapter 52
+
+
