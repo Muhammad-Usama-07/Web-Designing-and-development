7100da6f4bba18b6c055449f59cbdfc7697466f5
Round Robin <Round@gmail.com>
2020-07-10 06:53:48 +0500

    getting id

--- Js and work (part2)/StopWatch Assign/stopwatch.js
+++ Js and work (part2)/StopWatch Assign/stopwatch.js
@@ -1,3 +1,7 @@
 var min = 0;
 var sec = 0;
 var msec = 0;
+var minheading  = document.getElementById('min');
+var secheading  = document.getElementById('sec');
+var msecheading  = document.getElementById('msec');
+var interval;
