f0a8be073cdaf174a8a9ab1c72d1d5fac2fae989
Your Name <Kitti@gmail.com>
2020-07-17 14:35:07 +0500

    Done Question 4 Chapter 38-42

--- Js and work (part2)/Chapter 38-42 Assign/app.js
+++ Js and work (part2)/Chapter 38-42 Assign/app.js
@@ -6,10 +6,10 @@ function power(a, b) {
     alert(a)
 }
 power(2, 3);
-*/
+
 
 // Question # 2
-/*
+
 function leapYear(year){
     var result; 
     if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
@@ -21,7 +21,7 @@ function leapYear(year){
  }
 var year = prompt("Enter year: ");
 leapYear(year)
-*/
+
 // Question # 3
 
 function triarea(a,b,c){
@@ -31,4 +31,64 @@ function triarea(a,b,c){
 }
 
 triarea(1,3,3);
+*/
+
+// Question # 4
+
+
+function average(sub1, sub2, sub3){
+    var avg = (sub1+sub2+sub3)/3;
+    return avg;
+}
+function percentage(){
+    var perc = average(sub1,sub2,sub3)+"%";
+    return perc
+}
+function mainfunction(sub1, sub2, sub3){
+    alert("The average = "+average(sub1, sub2, sub3))
+    alert("The percentage = "+percentage(sub1, sub2, sub3))
+}
+
+var sub1 = parseInt(prompt("Enter marks in subject 1"));
+var sub2 = parseInt(prompt("Enter marks in subject 2"));
+var sub3 = parseInt(prompt("Enter marks in subject 3"));
+mainfunction(sub1, sub2, sub3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
