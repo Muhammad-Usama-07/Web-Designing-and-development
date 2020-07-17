d986b108c29ab08c153aa46bc34a2d7fe02caf86
Your Name <Kitti@gmail.com>
2020-07-17 13:59:11 +0500

    Question 2 Chapter 38-42

--- Js and work (part2)/Chapter 38-42 Assign/app.js
+++ Js and work (part2)/Chapter 38-42 Assign/app.js
@@ -1,8 +1,23 @@
 // Starting Chapter 38-42
-
-// Question 1
+/*
+// Question # 1
 function power(a, b) {
     a = a ** b;
     alert(a)
 }
 power(2, 3);
+*/
+
+// Question # 2
+
+function leapYear(year){
+    var result; 
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+      alert("leap")
+    }
+    else{
+      alert("not leep")
+    }
+ }
+var year = prompt("Enter year: ");
+leapYear(year)
