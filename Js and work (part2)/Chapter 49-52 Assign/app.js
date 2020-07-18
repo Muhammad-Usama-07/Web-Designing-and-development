91794e9e55c16b584124ab2314009314853f5c66
Your Name <Kitti@gmail.com>
2020-07-18 15:30:19 +0500

    Done Question 2 Chapter 43-48

--- Js and work (part2)/Chapter 49-52 Assign/app.js
+++ Js and work (part2)/Chapter 49-52 Assign/app.js
@@ -13,3 +13,12 @@ function gdet(){
     document.write('Your contact: '+cont.value+"<br>")
 }
 */
+
+// Question # 02
+function readmore() {
+    var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scel erisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta."
+    var pretext = document.getElementById("para");
+    pretext.innerHTML = text;
+    
+    
+}
