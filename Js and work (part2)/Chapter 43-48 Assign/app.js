d3b37b24476593175cf164e0bb31c7f43c693efd
Your Name <Kitti@gmail.com>
2020-07-18 10:20:07 +0500

    Done Question 4 Chapter 43-48

--- Js and work (part2)/Chapter 43-48 Assign/app.js
+++ Js and work (part2)/Chapter 43-48 Assign/app.js
@@ -1,6 +1,19 @@
 // Starting Chapter 43-48
-/**/
+/*
 function deleteRow(r) {
   var i = r.parentNode.parentNode.rowIndex;
   document.getElementById("thistable").deleteRow(i);
+}
+*/
+
+// Question # 4 
+
+function chnageimage(){
+    var chnageimg = document.getElementById('firstimage');
+    chnageimg.src = "images/mib2.png";
+}
+
+function previmg(){
+    var previmgg = document.getElementById('firstimage');
+    previmgg.src = "images/mib1.png";
 }
