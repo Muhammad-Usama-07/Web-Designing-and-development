c3131777707ea2eb98defa6129bc77971120f6fa
Your Name <Kitti@gmail.com>
2020-07-19 20:35:53 +0500

    Getting text node

--- Class19/app.js
+++ Class19/app.js
@@ -1,7 +1,9 @@
 var list = document.getElementById('list')
 
 function addtodo(){
-    var todo = document.getElementById('todo-item')
-    var li = document.createElement('li')
+    var todo = document.getElementById('todo-item');
+    var li = document.createElement('li');
+    var litest = document.createTextNode('Ghous')
+    li.appendChild(litest)
     console.log(li)
 }
