d0361221712f4b3bed4e89588b6045a95780be6e
Your Name <Kitti@gmail.com>
2020-07-19 20:38:29 +0500

    Getting text from input and printing in consol

--- Class19/app.js
+++ Class19/app.js
@@ -1,9 +1,9 @@
 var list = document.getElementById('list')
 
 function addtodo(){
-    var todo = document.getElementById('todo-item');
+    var todoitem = document.getElementById('todo-item');
     var li = document.createElement('li');
-    var litest = document.createTextNode('Ghous')
+    var litest = document.createTextNode(todoitem.value)
     li.appendChild(litest)
     console.log(li)
 }
