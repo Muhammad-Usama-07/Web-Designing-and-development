180b92eb4a721d5541d91c2d8a29aa949247e1bd
Your Name <Kitti@gmail.com>
2020-07-19 20:46:27 +0500

    creating delete button

--- Class19/app.js
+++ Class19/app.js
@@ -2,9 +2,17 @@ var list = document.getElementById('list')
 
 function addtodo(){
     var todoitem = document.getElementById('todo-item');
+    
+    // creating li tag with text node 
     var li = document.createElement('li');
     var litest = document.createTextNode(todoitem.value)
     li.appendChild(litest);
+    
+    // creating delet button  
+    var dltbtn = document.createElement("button");
+    var dlttext = document.createTextNode("DELETE");
+    dltbtn.appendChild(dlttext)
+    
     list.appendChild(li);
     todoitem.value = "";
     console.log(li)
