b271781c50f9bbd59da206867d95ee31db6b5968
Your Name <Kitti@gmail.com>
2020-07-21 11:50:25 +0500

    Done Question 1 Chapter 53-58

--- Js and work (part2)/Chapter 53-58 Assign/app.js
+++ Js and work (part2)/Chapter 53-58 Assign/app.js
@@ -1,10 +1,6 @@
-var myimage = document.getElementById("modal-img");
-myimage = ['images/1.jpg', 'images/3.jpg', 'images/5.jpg', 'images/6.jpg']
 
-
-function onClosedImagModal(){
-    var modal = document.getElementById('modal');
-    modal.classList.remove('modal-close');
-    modal.classList.add('modal-open')
-    setTimeout(()=>{ modal.style.display = "none"; }, 550)
+function showiage(e){
+    console.log(e)
+    var modelimg = document.getElementById('modelimage');
+    modelimg.src = e.src;
 }
