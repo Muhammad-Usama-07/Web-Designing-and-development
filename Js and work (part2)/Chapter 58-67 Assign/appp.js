442ca927c0f6979cf7faa913138800716c0efaff
Your Name <Kitti@gmail.com>
2020-07-23 10:39:30 +0500

    Done Step (iii) Question 1 Chapter 58-67

--- Js and work (part2)/Chapter 58-67 Assign/appp.js
+++ Js and work (part2)/Chapter 58-67 Assign/appp.js
@@ -1,8 +1,16 @@
+// Step (i) (ii) done
+/*
 var a = document.getElementById('main-content');
 console.log(a.childNodes);
+*/
+// Step (iii) done
 
+var a = document.getElementById('main-content');
+var renderr = a.getElementsByClassName('render')
 
-
+for(var i= 0; i<=renderr.length-1; i++){
+    console.log(renderr[i].innerHTML)
+}
 
 
 
