e2cbeb8f1ec6eac2f37c15dc988301ae490194ed
Your Name <Kitti@gmail.com>
2020-07-23 10:44:05 +0500

    Done Step (v) Question 1 Chapter 58-67

--- Js and work (part2)/Chapter 58-67 Assign/appp.js
+++ Js and work (part2)/Chapter 58-67 Assign/appp.js
@@ -1,3 +1,5 @@
+// Question # 1
+
 // Step (i) (ii) done
 /*
 var a = document.getElementById('main-content');
@@ -11,14 +13,23 @@ var renderr = a.getElementsByClassName('render')
 for(var i= 0; i<=renderr.length-1; i++){
     console.log(renderr[i].innerHTML)
 }
-*/
+
 // Step (iv)
 
 var firstname = document.getElementById('first-name');
 
 firstname.value = "Muhammad Usama";
+*/
 
+// Step (v)
 
+var firstname = document.getElementById('first-name');
+var lastname = document.getElementById('last-name');
+var email = document.getElementById('email');
+
+firstname.value = "Muhammad Usama";
+lastname.value = "Rajput";
+email.value = "abcss@gmail.com";
 
 
 
