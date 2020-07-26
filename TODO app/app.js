cc7c838303b887461a0b40a677f891cbad8cbd6d
Your Name <Kitti@gmail.com>
2020-07-26 16:59:54 +0500

    Changing html and JS

--- TODO app/app.js
+++ TODO app/app.js
@@ -1,15 +1,19 @@
-var list = document.getElementById('list')
+var list = document.getElementById('list');
 
 function addtodo(){
     var todoitem = document.getElementById('todo-item');
-    
-    // creating li tag with text node 
+    if(todoitem.value == ""){
+        alert("Please enter any value")
+    }
+    else{
+        // creating li tag with text node 
     var li = document.createElement('li');
     var litest = document.createTextNode(todoitem.value)
     li.appendChild(litest);
     
     // creating delet button  
     var dltbtn = document.createElement("button");
+    dltbtn.style.marginLeft = "88px";
     var dlttext = document.createTextNode("DELETE");
     dltbtn.appendChild(dlttext);
     dltbtn.setAttribute("class","btn");
@@ -17,7 +21,8 @@ function addtodo(){
     
     
     var editbtn = document.createElement("button");
-    var edittext = document.createTextNode("Edit");
+    editbtn.style.marginLeft = "10px";
+    var edittext = document.createTextNode("Edit");    
     editbtn.appendChild(edittext);
     editbtn.setAttribute("onclick", "edititem(this)")
     
@@ -27,6 +32,9 @@ function addtodo(){
     list.appendChild(li);
     todoitem.value = "";
     console.log(li)
+    }
+    
+    
 }
 
 function deletbtun(e){
