1c73304f5cc80b917600a83515129b09777487b2
Your Name <Kitti@gmail.com>
2020-08-20 12:30:59 +0500

    Working in class 21

---
+++ Class 20/Class20.js
@@ -0,0 +1,46 @@
+var question = [{
+        id: 1,
+        question: "What is the Fullform of HTML?",
+        answer: "Hypertext markup language",
+        options: [
+            "Hypertype markup language",
+            "Hypertext markin language",
+            "Hypertext markdown language",
+            "Hypertext manage language"
+        ]
+    },
+    {
+        id: 2,
+        question: "What is the Fullform of CSS?",
+        answer: "Casecading stylesheet",
+        options: [
+            "case style sheet",
+            "case sheet style",
+            "Casecading stylesheet",
+            "casecading sheet style"
+        ]
+    },
+    {
+        id: 3,
+        question: "What is the Fullform of JS",
+        answer: "Javascript",
+        options: [
+            "Hypertype markup language",
+            "Hypertext markin language",
+            "Hypertext markdown language",
+            "Hypertext manage language"
+        ]
+    },
+    {
+        id: 4,
+        question: "What is the Fullform of HTML?",
+        answer: "Hypertext markup language",
+        options: [
+            "Hypertype markup language",
+            "Hypertext markin language",
+            "Hypertext markdown language",
+            "Hypertext manage language"
+        ]
+    }
+]
+
