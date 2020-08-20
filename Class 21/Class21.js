0f3c581b3ccfa1cbb59b087577ba32c170f59721
Your Name <Kitti@gmail.com>
2020-08-20 12:48:15 +0500

    function in Constructor

--- Class 21/Class21.js
+++ Class 21/Class21.js
@@ -3,6 +3,10 @@
 function Student(name, roll) {
     this.name = name;
     this.roll = roll
+    this.getname = function() {
+        return name;
+    }
 }
 
 var student = new Student("ghous", 123)
+console.log(student.getname())
