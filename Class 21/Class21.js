8a19bcbfda1d2f7f78845c4403f1887729c80bf6
Your Name <Kitti@gmail.com>
2020-08-20 16:09:40 +0500

    Read write data with firebase

--- Class 21/Class21.js
+++ Class 21/Class21.js
@@ -64,3 +64,14 @@ var student = {
 console.log(student.hasOwnProperty("name2"))
 
 */
+
+function saveData() {
+    var name = document.getElementById('name');
+    var roll = document.getElementById('roll');
+    var student = {
+        name: name.value,
+        roll: roll.value
+
+    }
+    firebase.database().ref('student').set(student);
+}
