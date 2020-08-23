f5933876ab17d4272e6de73da3c58daf982532b4
Your Name <Kitti@gmail.com>
2020-08-23 14:14:33 +0500

    not coming back to web function

--- Class 22/Class22.js
+++ Class 22/Class22.js
@@ -1,5 +1,7 @@
 function press() {
     var locate = window.location.href
         //console.log(locate)
-    window.location.assign('https://www.google.com/')
+        //window.location.assign('https://www.google.com/')
+    window.location.replace('https://www.google.com/')
+
 }
