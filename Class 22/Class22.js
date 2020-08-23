ae066bb044604ecb1c4bc7e5bb5cb70cfea12081
Your Name <Kitti@gmail.com>
2020-08-23 14:40:24 +0500

    Assigning name width and height to window.open()

--- Class 22/Class22.js
+++ Class 22/Class22.js
@@ -7,6 +7,6 @@ function press() {
         // window.location.reload(true) // server reloading.
     var contend = "you Login "
         // window.open().document.write(contend)
-    window.open('https://www.google.com/')
+    window.open('https://www.google.com/', 'win2', 'width = 300, height = 300')
 
 }
