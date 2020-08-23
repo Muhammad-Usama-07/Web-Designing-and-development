a46ad0b776844c76b5f2d57a3f4350ec02ec0981
Your Name <Kitti@gmail.com>
2020-08-23 14:20:56 +0500

    Reloading page

--- Class 22/Class22.js
+++ Class 22/Class22.js
@@ -2,6 +2,8 @@ function press() {
     var locate = window.location.href
         //console.log(locate)
         //window.location.assign('https://www.google.com/')
-    window.location.replace('https://www.google.com/')
+        //window.location.replace('https://www.google.com/')
+    window.location.reload(false) // cache reloading.
+        // window.location.reload(true) // server reloading.
 
 }
