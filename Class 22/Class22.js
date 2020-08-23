5ad233f336d04e4e0f0bd30995e0bb0265c38950
Your Name <Kitti@gmail.com>
2020-08-23 14:33:15 +0500

    Open a blank window

--- Class 22/Class22.js
+++ Class 22/Class22.js
@@ -3,7 +3,7 @@ function press() {
         //console.log(locate)
         //window.location.assign('https://www.google.com/')
         //window.location.replace('https://www.google.com/')
-    window.location.reload(false) // cache reloading.
+        // window.location.reload(false) // cache reloading.
         // window.location.reload(true) // server reloading.
-
+    window.open()
 }
