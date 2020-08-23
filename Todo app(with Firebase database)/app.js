89d92a5bf62f6526f19e6d54879292dcc2a2cbcb
Your Name <Kitti@gmail.com>
2020-08-23 12:32:55 +0500

    Getting firebase data and daplaying to app

--- Todo app(with Firebase database)/app.js
+++ Todo app(with Firebase database)/app.js
@@ -16,6 +16,36 @@ function addtodo() {
         firebase.database().ref('Todo app data/' + key).set(student)
             // Data Added //
 
+        // Get Data from firebase
+        // ********************** 
+        var task;
+        var keyy;
+        var arrr = "";
+        var ref = firebase.database().ref('Todo app data');
+        ref.on('value', gotdata)
+
+        function gotdata(data) {
+            console.log(data.val())
+            var todoappdata = data.val();
+            var keyss = Object.keys(todoappdata);
+            console.log(keyss);
+            for (var i = 0; i < keyss.length; i++) {
+                var k = keyss[i];
+                task = todoappdata[k].Task;
+                keyy = todoappdata[k].key;
+                console.log(task, keyy);
+                if (task == todoitem.value) {
+                    console.log(task)
+                    arrr = task;
+                }
+
+            }
+
+        }
+        //***********************/
+
+
+
         // creating li tag with text node 
         var li = document.createElement('li');
         var litest = document.createTextNode(todoitem.value)
