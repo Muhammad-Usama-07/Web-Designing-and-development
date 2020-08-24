60a3c91a2526fca671749029e6d1fb5351978ec7
Your Name <Kitti@gmail.com>
2020-08-24 23:31:17 +0500

    (Method 1) Concatinate to arrays

--- Class 23/Class23.js
+++ Class 23/Class23.js
@@ -3,3 +3,11 @@
 // let firstname = "Usama";
 // let lasttname = "Rajput";
 // console.log(`FirstName: ${firstname}\nLastName: ${lasttname} `)
+
+// Spread Operator
+
+var student1 = ["ghous", "basit"];
+var student2 = ["ahmed", "umair"];
+
+var merge = student1.concat(student2);
+console.log(merge)
