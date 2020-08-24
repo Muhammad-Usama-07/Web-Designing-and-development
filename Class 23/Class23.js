47f603680ee93ea4946f0c0ecff21a12ee5d0e45
Your Name <Kitti@gmail.com>
2020-08-24 23:36:33 +0500

    (Method 2 using Speard Operator) Concatinate Object

--- Class 23/Class23.js
+++ Class 23/Class23.js
@@ -6,8 +6,15 @@
 
 // Spread Operator
 
-var student1 = ["ghous", "basit"];
-var student2 = [...student1, "ahmed", "umair"];
+// var student1 = ["ghous", "basit"];
+// var student2 = [...student1, "ahmed", "umair"];
+
+// // var merge = student1.concat(student2);
+// console.log(student2)
+
+
+var student1 = { name: "ghous" };
+var student2 = {...student1, school: "Saylani" };
 
 // var merge = student1.concat(student2);
 console.log(student2)
