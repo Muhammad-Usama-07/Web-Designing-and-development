d1994ca6b8a67918f272cd4e295c305b4a7045ed
Your Name <Kitti@gmail.com>
2020-09-11 15:15:19 +0500

    Commenting code

--- Class 24/Class24.js
+++ Class 24/Class24.js
@@ -49,8 +49,8 @@
 
 // console.log(filter)
 
-let arr = [{ name: "ghous", age: 30 }, { name: "basit", age: 20 }]
+// let arr = [{ name: "ghous", age: 30 }, { name: "basit", age: 20 }]
 
-let filter = arr.filter(a => a.age > 10)
+// let filter = arr.filter(a => a.age > 10)
 
-console.log(filter)
+// console.log(filter)
