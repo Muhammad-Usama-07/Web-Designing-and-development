796ed74fea4b7951ece612588056167c6d32492b
Your Name <Kitti@gmail.com>
2020-09-12 09:17:55 +0500

    Getting email and password from user using firebase Authentication

--- Class 24/FirebaseAuthentication/app.js
+++ Class 24/FirebaseAuthentication/app.js
@@ -1,6 +1,15 @@
 let signup = () => {
     let email = document.getElementById("email");
     let password = document.getElementById("pass");
-
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+        .then((result) => {
+            console.log(result)
+        })
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage)
+        });
 
 }
